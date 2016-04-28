2b623f0027529a444d8e8a2ed4a217ce134f9a90
Chris Gaona <thebeloved88@sbcglobal.net>
2016-04-27 18:50:06 -0700

    Adds initial quotes json object

--- js/script.js
+++ js/script.js
@@ -1,9 +1,61 @@
 (function() {
   'use strict';
 
-  var quotes = {};
-// event listener to respond to clicks on the page
-// when user clicks anywhere on the page, the "makeQuote" function is called
+  //adds quotes variable to contain json object of 7 quotes
+  var quotes = {[
+    {
+      quote: 'Some of the worst mistakes of my life have been haircuts.',
+      source: 'Jim Morrison',
+      citation: '',
+      year: '',
+      category: ''
+    },
+    {
+      quote: 'quote 2',
+      source: '',
+      citation: '',
+      year: '',
+      category: ''
+    },
+    {
+      quote: 'quote 3',
+      source: '',
+      citation: '',
+      year: '',
+      category: ''
+    },
+    {
+      quote: 'quote 4',
+      source: '',
+      citation: '',
+      year: '',
+      category: ''
+    },
+    {
+      quote: 'quote 5',
+      source: '',
+      citation: '',
+      year: '',
+      category: ''
+    },
+    {
+      quote: 'quote 6',
+      source: '',
+      citation: '',
+      year: '',
+      category: ''
+    },
+    {
+      quote: 'quote 7',
+      source: '',
+      citation: '',
+      year: '',
+      category: ''
+    },
+  ]};
+
+  // event listener to respond to clicks on the page
+  // when user clicks anywhere on the page, the "makeQuote" function is called
   document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 })();
