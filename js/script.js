026eb3a54e3e3ea379ee558ae6292fc069b74728
Chris Gaona <thebeloved88@sbcglobal.net>
2016-04-27 19:03:20 -0700

    Adds 7 quotes for random generation

--- js/script.js
+++ js/script.js
@@ -6,51 +6,51 @@
     {
       quote: 'Some of the worst mistakes of my life have been haircuts.',
       source: 'Jim Morrison',
-      citation: '',
-      year: '',
-      category: ''
+      citation: 'No One Here Gets Out Alive',
+      year: '1950',
+      category: ['life', 'hair']
     },
     {
-      quote: 'quote 2',
-      source: '',
-      citation: '',
-      year: '',
-      category: ''
+      quote: 'Gratitude is not only the greatest of virtues, but the parent of all others.',
+      source: 'Cicero',
+      citation: 'Pro Plancio',
+      year: '54 B.C.',
+      category: ['gratitude', 'thankfulness', 'life']
     },
     {
-      quote: 'quote 3',
-      source: '',
-      citation: '',
-      year: '',
-      category: ''
+      quote: 'Electricity is actually made up of extremely tiny particles called electrons, that you cannot see with the naked eye unless you have been drinking.',
+      source: 'Dave Barry',
+      citation: 'The Taming of the Screw',
+      year: '1972',
+      category: ['drinking', 'funny']
     },
     {
-      quote: 'quote 4',
-      source: '',
+      quote: 'Be a first rate version of yourself, not a second rate version of someone else.',
+      source: 'Judy Garland',
       citation: '',
       year: '',
-      category: ''
+      category: ['life']
     },
     {
-      quote: 'quote 5',
-      source: '',
-      citation: '',
+      quote: 'I\'ve always thought that a big laugh is a really loud noise from the soul saying, "Ain\'t that the truth."',
+      source: 'Quincy Jones',
+      citation: 'Victory of the Spirit',
       year: '',
-      category: ''
+      category: ['laugh', 'life', 'funny']
     },
     {
-      quote: 'quote 6',
-      source: '',
+      quote: 'You can go a long way with a smile. You can go a lot farther with a smile and a gun.',
+      source: 'Al Capone',
       citation: '',
-      year: '',
-      category: ''
+      year: '1935',
+      category: ['life', 'funny']
     },
     {
-      quote: 'quote 7',
-      source: '',
-      citation: '',
-      year: '',
-      category: ''
+      quote: 'Put your whole self into it, and you will find your true voice. Hold back and you won\'t. It\'s that simple.',
+      source: 'Hugh Macleod',
+      citation: 'How To Be Creative: 25. You have to find your own schtick',
+      year: '2004',
+      category: ['life']
     },
   ]};
 
