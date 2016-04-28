036406a0691e746046dd962f1b61d98b8254a7b1
Chris Gaona <thebeloved88@sbcglobal.net>
2016-04-28 07:50:19 -0700

    Adds shares & color key/value pairs; color for changing background

--- js/script.js
+++ js/script.js
@@ -8,49 +8,63 @@
       source: 'Jim Morrison',
       citation: 'No One Here Gets Out Alive',
       year: '1950',
-      category: ['life', 'hair']
+      shares: '25',
+      category: ['life', 'hair'],
+      color: 'red'
     },
     {
       quote: 'Gratitude is not only the greatest of virtues, but the parent of all others.',
       source: 'Cicero',
       citation: 'Pro Plancio',
       year: '54 B.C.',
-      category: ['gratitude', 'thankfulness', 'life']
+      shares: '250',
+      category: ['gratitude', 'thankfulness', 'life'],
+      color: 'yellow'
     },
     {
       quote: 'Don\'t cry because it\'s over, smile because it happened.',
       source: 'Dr. Seuss',
       citation: '',
       year: '1925',
-      category: ['drinking', 'funny']
+      shares: '12',
+      category: ['drinking', 'funny'],
+      color: 'green'
     },
     {
       quote: 'Be a first rate version of yourself, not a second rate version of someone else.',
       source: 'Judy Garland',
       citation: '',
       year: '',
-      category: ['life']
+      shares: '137',
+      category: ['life'],
+      color: 'purple'
     },
     {
       quote: 'I\'ve always thought that a big laugh is a really loud noise from the soul saying, "Ain\'t that the truth."',
       source: 'Quincy Jones',
       citation: 'Victory of the Spirit',
       year: '',
-      category: ['laugh', 'life', 'funny']
+      shares: '4',
+      category: ['laugh', 'life', 'funny'],
+      color: 'pink'
     },
     {
       quote: 'You can go a long way with a smile. You can go a lot farther with a smile and a gun.',
       source: 'Al Capone',
       citation: '',
       year: '1935',
-      category: ['life', 'funny']
+      shares: '95',
+      category: ['life', 'funny'],
+      color: 'orange'
     },
     {
       quote: 'Put your whole self into it, and you will find your true voice. Hold back and you won\'t. It\'s that simple.',
       source: 'Hugh Macleod',
       citation: 'How To Be Creative: 25',
       year: '2004',
-      category: ['life']
+      shares: '64',
+      category: ['life'],
+      color: 'blue'
     },
   ];
 
@@ -85,6 +99,8 @@
     } else {
       string = '<p class="quote">' + quote.quote + '</p> <p class="source">' + quote.source + '<span class="citation">' + quote.citation + '</span><span class="year">' + quote.year + '</span><span class="category">' + quote.category + '</span></p>';
     }
+
+    document.body.style.background = quote.color;
 
     //add string from if statement to innerHTML of quote-box
     document.getElementById('quote-box').innerHTML = string;
