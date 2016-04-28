08e09c8ae73613dce6eee9f45c89da3e37e7767c
Chris Gaona <thebeloved88@sbcglobal.net>
2016-04-27 18:34:44 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  var quotes = {};
+// event listener to respond to clicks on the page
+// when user clicks anywhere on the page, the "makeQuote" function is called
+  document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+})();
